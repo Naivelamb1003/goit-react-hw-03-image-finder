bdd2f363be4cdceb9d1a5c3e3f718deaccb9bc8f
Malik Nadiia <59849318+Naivelamb1003@users.noreply.github.com>
2021-02-03 22:03:31 +0200

    закончен рендер изображений

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,18 +1,67 @@
 import PropTypes from "prop-types";
-import ImageGalleryItem from "../Modal/Modal";
- import styles from './ImageGallery.module.css';
+import ImageGalleryItem from "../ImageGalleryItem/ImageGalleryItem";
+import styles from "./ImageGallery.module.css";
+import { Component } from "react";
+import ImageAPI from "../../services/API"
 
-const ImageGallery = ({  }) => {
-    return (
-        <ul className={styles.ImageGallery}>
-          <ImageGalleryItem/>
-        </ul>
+const Status = {
+  IDLE: 'idle',
+  PENDING: 'pending',
+  RESOLVED: 'resolved',
+  REJECTED: 'rejected',
+};
+
+class ImageGallery extends Component {
+
+  state = {
+    image: null,
+    error: null,
+    status: Status.IDLE,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const prevSearch = prevProps.searchQuery;
+    const nextSearch = this.props.searchQuery;
+  
+    if (prevSearch !== nextSearch) {
+      this.setState({ status: Status.PENDING });
+
+      setTimeout(() => {
+        ImageAPI
+          .fetchImage(nextSearch)
+          .then(image => this.setState({ image, status: Status.RESOLVED }))
+          .catch(error => this.setState({ error, status: Status.REJECTED }));
+      }, 2000);
+    }
+  }
+
+  render() {
+
+    const { image, error, status } = this.state;
+    const { searchQuery } = this.props;
+
+    if (status === 'idle') {
+      return <div>Введите название изображения.</div>;
+    }
+
+    if (status === 'pending') {
+      return <span>Loading...</span> ;
+    }
+
+    if (status === 'rejected') {
+      return alert('Ошибка в поиске изображения') ;
+    }
+
+    if (status === 'resolved') {
+      return (
+      <ul className={styles.ImageGallery}>
+        <ImageGalleryItem />
+      </ul>
     );
-  };
-  
-  ImageGallery.propTypes = {
-    onSubmit: PropTypes.func,
+    }
+  }
     
-  };
-  
-  export default ImageGallery;
+  }
+
+
+export default ImageGallery;
---
+++ src/services/API.js
@@ -0,0 +1,19 @@
+function fetchImage(searchQuery) {
+  const URL = "https://pixabay.com/api/";
+  const APIKEY = "19280898-6ce77fd5c708cfadb8184ec4d";
+  return fetch(
+    `${URL}?q=${searchQuery}&page=1&key=${APIKEY}&image_type=photo&orientation=horizontal&per_page=12`
+  ).then((response) => {
+    if(response.ok){
+        return response.json();
+    }
+    return Promise.reject(new Error(`Не найдено изображение с таким именем ${searchQuery}`));
+    
+  });
+}
+
+const api = {
+  fetchImage,
+};
+
+export default api;
